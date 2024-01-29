93ed11f25808728c4f9b069d4a7751bc5fe01113
Gudmon <gudmonmarcell@gmail.com>
2024-01-29 18:47:43 +0100

    add: not found page

--- calculator/src/app/components/not-found/not-found.component.ts
+++ calculator/src/app/components/not-found/not-found.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from "../navigation/navigation.component";
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-not-found',
-  standalone: true,
-  imports: [],
-  templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.css'
+    selector: 'app-not-found',
+    standalone: true,
+    templateUrl: './not-found.component.html',
+    styleUrl: './not-found.component.css',
+    imports: [NavigationComponent]
 })
 export class NotFoundComponent {
+    constructor(readonly router: Router) {}
 
+    navigateToHome(){
+        this.router.navigate(['/']);
+    }
 }
