1ffcb7022a40c324af4bc3371ef3579ebab03119
Gudmon <gudmonmarcell@gmail.com>
2024-04-17 14:22:16 +0200

    add: cookie consent

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component  } from '@angular/core';
+import { Component, OnDestroy, OnInit  } from '@angular/core';
 import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ProductListComponent } from "./components/product-list/product-list.component";
 import { MenubarModule } from 'primeng/menubar';
@@ -9,8 +9,9 @@ import { FooterComponent } from './components/footer/footer.component';
 import { LoadingService } from './services/loading.service';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { CommonModule } from '@angular/common';
-
-
+import {NgcCookieConsentModule, NgcCookieConsentConfig, NgcCookieConsentService, NgcInitializationErrorEvent, NgcStatusChangeEvent, NgcInitializingEvent, NgcNoCookieLawEvent} from 'ngx-cookieconsent';
+import { Subscription } from 'rxjs';
+declare var cookieconsent: any;
 @Component({
     selector: 'app-root',
     standalone: true,
@@ -18,15 +19,55 @@ import { CommonModule } from '@angular/common';
     styleUrl: './app.component.css',
     imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule, ProgressSpinnerModule, ProductListComponent, MenubarModule, NavigationComponent, LandingComponent, FooterComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   items: MenuItem[] | undefined;
   readonly loading$ = this.loadingService.status$;
 
   constructor(
     private readonly loadingService: LoadingService,
     private readonly router: Router){}
+    private scriptLoaded = false;
+    
+    initialize() {
+        if (typeof cookieconsent !== 'undefined') {
+          cookieconsent.run({
+            "notice_banner_type": "simple",
+            "consent_type": "express",
+            "palette": "dark",
+            "language": "hu",
+            "page_load_consent_levels": ["strictly-necessary"],
+            "notice_banner_reject_button_hide": false,
+            "preferences_center_close_button_hide": false,
+            "page_refresh_confirmation_buttons": false,
+            "website_name": "www.palmsmagyarorszag.hu"
+          });
+        } else {
+          console.error('Cookie Consent script not loaded.');
+        }
+    }
+
+    private loadScript(url: string, callback?: (ev: Event) => any): void {
+    const script = document.createElement('script');
+    script.src = url;
+    script.type = 'text/javascript';
+    script.charset = 'UTF-8';
+    script.async = true;
+    
+    if (callback) {
+        script.onload = callback.bind(this);
+    }
+    document.head.appendChild(script);
+    }
 
     ngOnInit() {
+         // Load Cookie Consent script
+        this.loadScript('https://www.termsfeed.com/public/cookie-consent/4.1.0/cookie-consent.js', this.initialize);
+
+        // Load Clarity script
+        this.loadScript('https://www.clarity.ms/tag/lxxyj7muce');
+
+        this.initialize();
+
         this.items = [
             {
                 label: 'Kezdőlap'
@@ -51,5 +92,6 @@ export class AppComponent {
             }  
             window.scrollTo(0, 0) 
         }); 
+
     }
 }
