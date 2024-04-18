1e7c30734ecf6a4b84a13460e23a6297acbfa2d3
Gudmon <gudmonmarcell@gmail.com>
2024-04-18 11:28:02 +0200

    delete: krpan

--- calculator/src/app/app.routes.ts
+++ calculator/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { OverviewComponent } from './components/calculator/krpan/components/overview/overview.component';
 import { ProductComponent } from './components/product/product.component';
 import { LandingComponent } from './landing/landing.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
@@ -11,14 +10,9 @@ import { PalmsTrailerPageComponent } from './components/calculator/palms/trailer
 
 export const routes: Routes = [
     {path: '', component: LandingComponent},
-    {path: 'calculator/krpan', component: OverviewComponent},
     {path: 'calculator/palms', component: PalmsOverviewComponent},
-    // {path: 'calculator/palms/trailers/:id', component: PalmsTrailerComponent},
     {path: 'calculator/palms/trailers/:id', component: PalmsTrailerPageComponent},
-    //{path: 'calculator/palms/cranes/:id', component: PalmsCraneComponent},
     {path: 'calculator/palms/cranes/:id', component: PalmsCranePageComponent},
-    
-    {path: 'krpan/:id', component: ProductComponent},
-    // {path: 'not-found', component: NotFoundComponent },
-    // {path: '**', redirectTo: '/not-found' } 
+    {path: 'not-found', component: NotFoundComponent },
+    {path: '**', redirectTo: '/not-found' } 
 ];
