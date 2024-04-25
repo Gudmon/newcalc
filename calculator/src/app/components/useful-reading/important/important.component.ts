906d6cb1f4d715182e5af400c80e2c300a157463
Gudmon <gudmonmarcell@gmail.com>
2024-04-25 18:18:33 +0200

    add: table to useful reading

--- calculator/src/app/components/useful-reading/important/important.component.ts
+++ calculator/src/app/components/useful-reading/important/important.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { FooterComponent } from "../../footer/footer.component";
+import { NavigationComponent } from "../../navigation/navigation.component";
+import { TableModule } from 'primeng/table';
 
 @Component({
-  selector: 'app-important',
-  standalone: true,
-  imports: [],
-  templateUrl: './important.component.html',
-  styleUrl: './important.component.css'
+    selector: 'app-important',
+    standalone: true,
+    templateUrl: './important.component.html',
+    styleUrl: './important.component.css',
+    imports: [FooterComponent, NavigationComponent, TableModule]
 })
-export class ImportantComponent {
+export class ImportantComponent implements OnInit {
+    products: any
+    public getScreenWidth: any;
+    public getScreenHeight: any;
 
+    smallScreen(){
+        return this.getScreenWidth < 645;
+    }
+
+    ngOnInit() {
+        this.getScreenWidth = window.innerWidth;
+        this.getScreenHeight = window.innerHeight;
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onWindowResize() {
+        this.getScreenWidth = window.innerWidth;
+        this.getScreenHeight = window.innerHeight;
+    }
 }
