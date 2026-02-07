0b13141f4aa45aa1c5a7ae668ad53eebc24507f7
Gudmon <gudmonmarcell@gmail.com>
2026-02-07 22:33:25 +0100

    feat: add cloudinary

--- calculator/src/app/app.component.ts
+++ calculator/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api/menuitem';
 import { LoadingService } from './services/loading.service';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { CommonModule } from '@angular/common';
+import { CloudinaryModule } from '@cloudinary/ng';
+import { Cloudinary, CloudinaryImage } from '@cloudinary/url-gen';
 
 declare var cookieconsent: any;
 @Component({
@@ -12,101 +14,118 @@ declare var cookieconsent: any;
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, CommonModule, ProgressSpinnerModule, MenubarModule]
+    imports: [RouterOutlet, CommonModule, ProgressSpinnerModule, MenubarModule, CloudinaryModule]
 })
 export class AppComponent implements OnInit {
-  items: MenuItem[] | undefined;
-  readonly loading$ = this.loadingService.status$;
+    items: MenuItem[] | undefined;
+    readonly loading$ = this.loadingService.status$;
 
-  private clarityScriptLoaded: boolean = false;
-  private clarityScriptTag: HTMLScriptElement | null = null;
+    private clarityScriptLoaded: boolean = false;
+    private clarityScriptTag: HTMLScriptElement | null = null;
 
-  constructor(
-    private readonly loadingService: LoadingService,
-    private readonly router: Router){}
+    constructor(
+        private readonly loadingService: LoadingService,
+        private readonly router: Router
+    ) {}
     private scriptLoaded = false;
-    
+
     initialize() {
         if (typeof cookieconsent !== 'undefined') {
-          cookieconsent.run({
-            "notice_banner_type": "simple",
-            "consent_type": "express",
-            "palette": "dark",
-            "language": "hu",
-            "page_load_consent_levels": ["strictly-necessary"],
-            "notice_banner_reject_button_hide": false,
-            "preferences_center_close_button_hide": false,
-            "page_refresh_confirmation_buttons": false,
-            "website_name": "www.palmsmagyarorszag.hu",
-            "callbacks": {
-                "notice_banner_loaded": () => {
-                },
-                "i_agree_button_clicked": (consent: any) => {     
-                    this.loadClarityScript("lxxyj7muce")
-                },
-                "i_decline_button_clicked": () => {
-                },
-                "change_my_preferences_button_clicked": () => {
-                },
-                "scripts_all_loaded": () => {
-                },
-                "user_consent_saved": (consent: Consent) => {
-                    // this.removeClarityScript("lxxyj7muce");
-                    // if(consent.tracking) {
-                    //     this.loadClarityScript("lxxyj7muce");
-                    // } 
-                },
-                "scripts_specific_loaded": (level: any) => {
-                    switch(level) {
-                        case 'strictly-necessary':
-                            break;
-                        case 'functionality':
-                            break;
-                        case 'tracking':
-                            this.loadClarityScript("lxxyj7muce");
-                            break;
-                        case 'targeting':
-                            break;
+            cookieconsent.run({
+                notice_banner_type: 'simple',
+                consent_type: 'express',
+                palette: 'dark',
+                language: 'hu',
+                page_load_consent_levels: ['strictly-necessary'],
+                notice_banner_reject_button_hide: false,
+                preferences_center_close_button_hide: false,
+                page_refresh_confirmation_buttons: false,
+                website_name: 'www.palmsmagyarorszag.hu',
+                callbacks: {
+                    notice_banner_loaded: () => {},
+                    i_agree_button_clicked: (consent: any) => {
+                        this.loadClarityScript('lxxyj7muce');
+                    },
+                    i_decline_button_clicked: () => {},
+                    change_my_preferences_button_clicked: () => {},
+                    scripts_all_loaded: () => {},
+                    user_consent_saved: (consent: Consent) => {
+                        // this.removeClarityScript("lxxyj7muce");
+                        // if(consent.tracking) {
+                        //     this.loadClarityScript("lxxyj7muce");
+                        // }
+                    },
+                    scripts_specific_loaded: (level: any) => {
+                        switch (level) {
+                            case 'strictly-necessary':
+                                break;
+                            case 'functionality':
+                                break;
+                            case 'tracking':
+                                this.loadClarityScript('lxxyj7muce');
+                                break;
+                            case 'targeting':
+                                break;
+                        }
                     }
-                }
-            },
-            "callbacks_force": true
-          });
+                },
+                callbacks_force: true
+            });
         } else {
-          console.error('Cookie Consent script not loaded.');
+            console.error('Cookie Consent script not loaded.');
         }
     }
-    
+
     loadClarityScript(tagId: string): void {
-        (function(c: any, l: any, a: string, r: string, i: string, t: any, y: any){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          t.charset = 'UTF-8';
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", tagId, document.createElement('script'), undefined);
+        (function (c: any, l: any, a: string, r: string, i: string, t: any, y: any) {
+            c[a] =
+                c[a] ||
+                function () {
+                    (c[a].q = c[a].q || []).push(arguments);
+                };
+            t = l.createElement(r);
+            t.async = 1;
+            t.src = 'https://www.clarity.ms/tag/' + i;
+            t.charset = 'UTF-8';
+            y = l.getElementsByTagName(r)[0];
+            y.parentNode.insertBefore(t, y);
+        })(window, document, 'clarity', 'script', tagId, document.createElement('script'), undefined);
     }
 
     removeClarityScript(tagId: string): void {
         const clarityScriptElement = document.getElementById(tagId);
         if (clarityScriptElement && clarityScriptElement.parentNode) {
-          clarityScriptElement.parentNode.removeChild(clarityScriptElement);
+            clarityScriptElement.parentNode.removeChild(clarityScriptElement);
         }
-      }
-      
-    private loadScript(url: string, callback?: (ev: Event) => any): void {
-    const script = document.createElement('script');
-    script.src = url;
-    script.type = 'text/javascript';
-    script.charset = 'UTF-8';
-    script.async = true;
-    
-    if (callback) {
-        script.onload = callback.bind(this);
-    }
-    document.head.appendChild(script);
     }
 
+    private loadScript(url: string, callback?: (ev: Event) => any): void {
+        const script = document.createElement('script');
+        script.src = url;
+        script.type = 'text/javascript';
+        script.charset = 'UTF-8';
+        script.async = true;
+
+        if (callback) {
+            script.onload = callback.bind(this);
+        }
+        document.head.appendChild(script);
+    }
+
+    //TODO: add for images
+    img!: CloudinaryImage;
     ngOnInit() {
+        // const cld = new Cloudinary({
+        //     cloud: {
+        //         cloudName: 'demo'
+        //     }
+        // });
+
+        // // Use the image with public ID, 'bike'.
+        // const myImage = cld.image('bike');
+
+        //this.img = new CloudinaryImage('PALMS-6S-2', { cloudName: 'dhidgc7eu' });
+
         this.loadScript('https://www.termsfeed.com/public/cookie-consent/4.1.0/cookie-consent.js', this.initialize);
 
         this.items = [
@@ -116,29 +135,28 @@ export class AppComponent implements OnInit {
             {
                 label: 'Kalkulátorok',
                 items: [
-                  {
-                      label: 'Krpan'
-                  },
-                  {
-                      label: 'Palms'
-                  }
-              ]
-            },
+                    {
+                        label: 'Krpan'
+                    },
+                    {
+                        label: 'Palms'
+                    }
+                ]
+            }
         ];
 
-        this.router.events.subscribe((event) => { 
-            if (!(event instanceof NavigationEnd)) { 
-                return; 
-            }  
-            window.scrollTo(0, 0) 
-        }); 
-
+        this.router.events.subscribe((event) => {
+            if (!(event instanceof NavigationEnd)) {
+                return;
+            }
+            window.scrollTo(0, 0);
+        });
     }
 }
 
-interface Consent{
-    "functionality": boolean,
-    "strictly-necessary": boolean,
-    "targeting": boolean,
-    "tracking": boolean
+interface Consent {
+    functionality: boolean;
+    'strictly-necessary': boolean;
+    targeting: boolean;
+    tracking: boolean;
 }
